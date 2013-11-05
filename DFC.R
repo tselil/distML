3aae04293a0804a00ca9f22ba8bd4a8a446be400
tselil <tselilschramm@gmail.com>
2013-11-05 10:37:08 -0800

    Added zeros

--- DFC.R
+++ DFC.R
@@ -11,12 +11,21 @@
 
 #slices <- ifelse(length(args) > 1, as.integer(args[[2]]),2)
 
-dims <- 10
+dims <- 30
 r <- 10
-testU <- matrix(rnorm(r*dims,mean = 0,sd = 1),r,dims)
-testV <- matrix(rnorm(r*dims,mean = 0,sd = 1),r,dims)
+testU <- matrix(rnorm(r*dims,mean = 4,sd = 1),r,dims)
+testV <- matrix(rnorm(r*dims,mean = 4,sd = 1),r,dims)
 testG <- matrix(rnorm(dims*dims,mean = 0,sd = .05),dims,dims)
 testM <- t(testV) %*% testU + testG
+
+zeros <- sample.int(dims^2, size=floor(0.7*dims^2)) 
+
+zeroedM <- testM
+for(i in zeros){
+	x = i %% dims
+	y = i %/% dims
+	zeroedM[x,y] <- 0
+}
 
 cat(testU,"\n\n")
 cat(testV,"\n\n")
@@ -26,7 +35,7 @@ sgdBase <- function(mat) {
 	# Set Parameters
 	m <- dim(mat)[1]
 	n <- dim(mat)[2]
-	lrate <- .04 # learning rate
+	lrate <- .004 # learning rate
 	k <- .04 # parameter used to minimize over-fitting
 	min_impr <- .0001 # min improvement
 	init <- .1 # initial value for features
@@ -75,6 +84,8 @@ sgdBase <- function(mat) {
 			# Calculate RMSE
 			rmse_prev <- rmse
 			rmse <- sqrt(sq_err/num_nonzeros)
+			cat("root mean squared error: ",rmse)
+			cat("\n")
 			impr <- rmse_prev - rmse
 			t <- t + 1
 		}
@@ -82,8 +93,9 @@ sgdBase <- function(mat) {
 	list(row_feats,col_feats)
 }
 
-feats <- sgdBase(testM)
+feats <- sgdBase(zeroedM)
 row_feats <- feats[[1]]
 col_feats <- feats[[2]]
 dif <- testM - (t(row_feats) %*% col_feats)
 cat("dif: ",dif)
+cat("\n")
