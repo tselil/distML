fb5e13a9c14d6d14c6224399496d7f3273e920e8
jonahbc <jonahibc@gmail.com>
2013-11-21 12:26:07 -0800

    Fixed syntax errors in the Rcpp matrix entry accesses.

--- maskUV.cpp
+++ maskUV.cpp
@@ -12,19 +12,23 @@ using namespace Rcpp;
 NumericVector maskUV(NumericMatrix U, NumericMatrix V, IntegerVector is, IntegerVector js)
 {
 	// Get the length of the entries list and the rank of UV'
-	l = is.size();
-	r = U.ncol();
+	int l = is.size();
+	int r = U.ncol();
 	
 	// Initialize the output vector to all zeros
 	NumericVector maskUV(l,0.0);
 	
 	// Loop over non-zero entries and compute output vector
+	int i = is(0);
+	int j = js(0);
 	for(int n = 0; n < l; n++)
 	{
+		i = is(n);
+		j = js(n);
 		maskUV(n) = 0;
 		for(int k = 0; k < r; k++)
 		{
-			maskUV(n) += U(is(n))(k)*V(js(n))(k);
+			maskUV(n) += U(i,k)*V(j,k);
 		}
 	}
 	return maskUV;
