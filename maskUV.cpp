4a2dcc2537858ad39c06938a6e572ca1d792bec1
jonahbc <jonahibc@gmail.com>
2013-11-21 14:56:24 -0800

    Fixed off-by-one bug. Now maskUV works correctly in tests.

--- maskUV.cpp
+++ maskUV.cpp
@@ -8,7 +8,7 @@ using namespace Rcpp;
 // U is an m-by-r matrix
 // V is an n-by-r matrix
 
-// [Rcpp::export]]
+// [[Rcpp::export]]
 NumericVector maskUV(NumericMatrix U, NumericMatrix V, IntegerVector is, IntegerVector js)
 {
 	// Get the length of the entries list and the rank of UV'
@@ -19,12 +19,12 @@ NumericVector maskUV(NumericMatrix U, NumericMatrix V, IntegerVector is, Integer
 	NumericVector maskUV(l,0.0);
 	
 	// Loop over non-zero entries and compute output vector
-	int i = is(0);
-	int j = js(0);
+	int i = is(1)-1;
+	int j = js(1)-1;
 	for(int n = 0; n < l; n++)
 	{
-		i = is(n);
-		j = js(n);
+		i = is(n)-1; // subtract 1 since R arrays start at 1
+		j = js(n)-1;
 		maskUV(n) = 0;
 		for(int k = 0; k < r; k++)
 		{
