017a36655588272f9734b864db3cc29ac649e718
bsweitz <bsweitz123@gmail.com>
2013-11-27 14:29:46 -0800

    updated wrapper

--- ExpWrapper.R
+++ ExpWrapper.R
@@ -4,18 +4,18 @@ require('Matrix')
 require(Rcpp)
 require(svd)
 
-args <- commandArgs(trailing = TRUE)
+upperArgs <- commandArgs(trailing = TRUE)
 
-if (length(args) < 1) {
+if (length(upperArgs) < 1) {
 	print("Usage: DFC.R <master> <lower_slices> <upper_slices> <masked_file> <lower_iterations> <upper_iterations> <output_file>")
 	q("no")
 }
-outfile <- paste(args[[7]],".out",sep="")
+outfile <- paste(upperArgs[[7]],".out",sep="")
 data <- list()
-for(i in seq(as.numeric(args[[2]]),as.numeric(args[[3]]))) { # num slices
-	for(j in seq(as.numeric(args[[5]]),as.numeric(args[[6]]),by = 5)) { # num iterations
-		master <- paste(args[[1]],"[",i,"]",sep="")
-		commandArgs <- function(trailing) list(master,i,args[[4]],j)
+for(i in seq(as.numeric(upperArgs[[2]]),as.numeric(upperArgs[[3]]))) { # num slices
+	for(j in seq(as.numeric(upperArgs[[5]]),as.numeric(upperArgs[[6]]),by = 5)) { # num iterations
+		master <- paste(upperArgs[[1]],"[",i,"]",sep="")
+		commandArgs <- function(trailing) list(master,i,upperArgs[[4]],j)
 		outs <- list(i,j,unlist(source("DFC.R")))
 		data <- rbind(data, outs)
 	}
