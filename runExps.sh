c55d85e23510f3c52cd1e8d21dd8af4a55bf7def
tselil <tselilschramm@gmail.com>
2013-12-02 12:18:22 -0800

    committing updated run experiments file

--- runExps.sh
+++ runExps.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
-MASTERURL = $(uname -n)
+#ec2-54-201-152-242.us-west-2.compute.amazonaws.com
 
-for FILE in ~/permanent-hdfs/datasets/*k/*
-	do
-		echo Running experiment for dataset ${FILE}
-		./sparkR ExpWrapper.R $MASTERURL 1 10 $FILE 10 150 10 ${FILE}.results
-	done
+
+for I in 1 2
+  do
+    for FILE in ~/persistent-hdfs/datasets/${I}k/*90_masked.out
+      do
+        for SLICES in 2 4 6 8 10
+          do
+            echo Running $SLICES slice experiment for dataset ${FILE}
+            ~/spark/sparkR ExpWrapper.R spark://ec2-54-201-100-85.us-west-2.compute.amazonaws.com:7077 $SLICES $SLICES $FILE 10 50 10 ${FILE}.slices${SLICES}.results
+            done
+      done
+  done
