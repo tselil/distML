e709ae28e7822d9b2d98cd319e90444ffd2b1433
tselil <tselilschramm@gmail.com>
2013-12-01 16:54:48 -0800

    blah

--- ExpWrapper.R
+++ ExpWrapper.R
@@ -7,7 +7,7 @@ require(svd)
 upperArgs <- commandArgs(trailing = TRUE)
 
 if (length(upperArgs) < 1) {
-	print("Usage: ExpWrapper.R <master> <lower_slices> <upper_slices> <masked_file> <lower_iterations> <upper_iterations> <output_file>")
+	print("Usage: ExpWrapper.R <master> <lower_slices> <upper_slices> <masked_file> <lower_iterations> <upper_iterations> <iteration_increment> <output_file>")
 	q("no")
 }
 outfile <- paste(upperArgs[[7]],".out",sep="")
@@ -18,7 +18,7 @@ if (master == "local"){
 }
 data <- c()
 for(i in seq(as.numeric(upperArgs[[2]]),as.numeric(upperArgs[[3]]))) { # num slices
-	for(j in seq(as.numeric(upperArgs[[5]]),as.numeric(upperArgs[[6]]),by = 5)) { # num iterations
+	for(j in seq(as.numeric(upperArgs[[5]]),as.numeric(upperArgs[[6]]),by = as.numeric(upperArgs[[7]]))) { # num iterations
 		ins <- list(master,i,upperArgs[[4]],j)
 		commandArgs <- function(trailing) ins
 		outs <- c(i,j,unlist(source("DFC.R")))
