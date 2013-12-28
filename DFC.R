15bff82ebcaa55a19a95958bd5e557e4eeffd09f
bsweitz <bsweitz123@gmail.com>
2013-12-28 11:30:57 -0800

    Cleaned up some garbage code and fixed a bug in errorCal

--- DFC.R
+++ DFC.R
@@ -1,19 +1,20 @@
+# Import required matrix and C interface packages
 require(SparkR)
 require(MASS)
 require('Matrix')
 require(Rcpp)
 require(svd)
 
+# Get the command line arguments
 args <- commandArgs(trailing = TRUE)
 
 if (length(args) < 1) {
-	print("Usage: DFC.R <master>[<slices>] <slices> <masked_file> <iterations> <randProject=T/F> <output_file>")
+	print("Usage: DFC.R <master>[<slices>] <slices> <masked_file> <iterations> <randProject=T/F>")
 	q("no")
 }
 
-# Takes a list of columns, makes a matrix and applies SGD algorithm
+# Takes a pair (matrix, iterations) and applies the base factorization algorithm for the specified iterations
 factorCols <- function(itersAndMat) {
-	#require('Matrix') # this is very important for some reason, probably should understand it
 	iters <- itersAndMat[[1]][[1]]
 	mat <- itersAndMat[[1]][[2]]
 	UV <- apgBase(mat,iters)
@@ -76,6 +77,7 @@ dfcProject <- function(factorList) {
 	list(X_A, X_B,projTime)
 }
 
+# Randomized Projection method for the C step of DFC
 dfcRandProject <- function(factorList) {
 	trproj <- proc.time()
 	V_1 <- factorList[[1]][[2]]
@@ -125,50 +127,59 @@ dfcRandProject <- function(factorList) {
 	list(Q,V,randprojTime) 
 }
 
-apgBase <- function(mat,maxiter,writeout = F, writeby = 10) {
-	tbase <- proc.time()
+# Accelerated Proximal Gradient algorithm for factoring.
+apgBase <- function(mat,maxiter) {
+	
+	tbase <- proc.time() # Timing code
+	
 	# load required packages
 	require('Matrix')
 	require(Rcpp)
 	require(svd)
+	
 	# Load and compile the fast C++ code
 	sourceCpp('maskUV.cpp')
-	# should figure out how to do all of the above better
+	
 	######## Set Initial Parameters #####################################
 	m <- dim(mat)[1]
 	n <- dim(mat)[2]
+	
 	IIJJ <- which(mat != 0,arr.ind = T) # list of nonzero indices
-	#p <- length(IIJJ) # number of nonzero entries
 	II <- IIJJ[,1] # nonzero row indices
 	JJ <- IIJJ[,2] # nonzero col indices
+	
 	L <- 1 # Lipschitz constant for 1/2*||Ax - b||_2^2
-	t <- 1 # speed time [BC13] parameter
+	t <- 1 
 	told <- t
 	beta <- 0 # beta = (told - 1)/t
 	num_sv <- 5 # number of SV to look at
-	num_pos_sv <-5 # initial number of positive singular values
+	num_pos_sv <- 5 # initial number of positive singular values
 	
-	U <- Matrix(0,m,1) # Factor of X
+	U <- Matrix(0,m,1) # Factor of mat
 	Uold <- U
-	V <- Matrix(0,n,1) # Factor of X
+	V <- Matrix(0,n,1) # Factor of mat
 	Vold <- V
-	mX <- sparseMatrix(m,n,x=0) # Sparse matrix containing predicted values
+	mX <- sparseMatrix(m,n,x=0) # Sparse matrix approximating mat
 	mXold <- mX # mX of previous iteration
 	mY <- mX # Sparse matrix "average" of Xold and X
 	
+	# SVD Truncation Parameters
 	mu0 <- norm(mat,type="F")
 	mu <- 0.1*mu0
 	muTarget <- 10^(-4)*mu0
 	cat("mu :", mu, "\n")
 	######################################################################
 
-	data <- c()
 	for(iter in 1:maxiter) {
 		cat("iteration: ",iter,"\n")
 		# Get query access to G = Y - 1/L*Grad
 		Grad <- mY - mat
-		f <- function(z) as.numeric((1+beta)*(U %*% (t(V) %*% z)) - beta*(Uold %*% (t(Vold) %*% z)) - 1/L*(Grad %*% z)) # query oracle to Gk
+		# query oracle to Gk
+		f <- function(z) as.numeric((1+beta)*(U %*% (t(V) %*% z)) - beta*(Uold %*% (t(Vold) %*% z)) - 1/L*(Grad %*% z))
+		# query oracle to Gk'
 		tf <- function(z) as.numeric((1+beta)*(V %*% (t(U) %*% z)) - beta*(Vold %*% (t(Uold) %*% z)) - 1/L*(t(Grad) %*% z))
+		
+		# Create External Matrix
 		G <- extmat(f, tf, m, n)
 		
 		# Compute partial SVD
@@ -180,6 +191,7 @@ apgBase <- function(mat,maxiter,writeout = F, writeby = 10) {
 		mXold <- mX
 		told <- t
 		
+		# Truncate the SV's and update the number of SV's to look at
 		s <- svd$d
 		Shlf <- sqrt(s[which(s > mu/L)])
 		if(num_sv == num_pos_sv) {
@@ -192,41 +204,35 @@ apgBase <- function(mat,maxiter,writeout = F, writeby = 10) {
 		# update number of positive singular values of X^k AFTER the above test
 		num_pos_sv <- length(Shlf)
 		
+		# Compute the factors U and V from the SVD
 		Sig <- diag(x = Shlf,num_pos_sv,num_pos_sv)
 		U <- (svd$u[,1:num_pos_sv] %*% Sig)
 		V <- (svd$v[,1:num_pos_sv] %*% Sig)
-		msUV <- maskUV(as.matrix(U),as.matrix(V),II,JJ)
-		mX <- sparseMatrix(i=II,j=JJ,x=msUV,dims=c(m,n)) # Call into C++ code
+		
+		# Compute mX = UV' using fast loops in C
+		msUV <- maskUV(as.matrix(U),as.matrix(V),II,JJ) # Call into C++ code
+		mX <- sparseMatrix(i=II,j=JJ,x=msUV,dims=c(m,n))
+		
+		# Update Parameters
 		t <- (1+sqrt(1+4*t^2))/2
 		beta <- (told - 1)/t
 		mY <- (1+beta)*mX - beta*mXold	
 		mu <- max(0.7*mu,muTarget)
 		cat("mu: ",mu,"\n")
-		apgtime <- as.numeric((proc.time() - tbase)["elapsed"])
-		
-		# Collect data every writeby iterations
-		if(writeout == TRUE & iter %% writeby == 0) {
-			rmse <- errorCal(mat,U,V)
-			outs <- c(1, iter, rmse, 0, apgtime, 0)
-			data <- rbind(data, outs)
-		}
 	}
+	# Output
+	apgtime <- as.numeric((proc.time() - tbase)["elapsed"]) #Timing Code
 	cat("U: ", dim(U),"\n")
 	cat("V: ", dim(V),"\n")
 	cat("RMSE for submatrix: ",errorCal(mat,U,V),"\n")
-	
-	# Write to a file
-	if(writeout == TRUE) {
-		print(data)
-		colnames(data) <- c("slices","iterations","RMSE","overhead","subproblem time","projection time")
-		write.matrix(data, file=outfile, sep="\t")
-	}
+
 	list(U,V,apgtime)
 }
 	
 
 # Base stochastic gradient descent algorithm for matrix completion
 sgdBase <- function(mat) {
+
 	# Set Parameters
 	m <- dim(mat)[1]
 	n <- dim(mat)[2]
@@ -252,49 +258,36 @@ sgdBase <- function(mat) {
 	nonzero_entries <- mat[nonzero_rowscols]
 	num_nonzeros <- length(nonzero_entries)
 	
-
-	
+	# Each iterate descends in the space of rank i matrices
 	for(i in 1:rank) {
 		cat("rank: ", i, "\n")
 		t <- 0
 		impr <- 0.0
+		# Descend as long as we can make improvements
 		while(t < min_itrs || impr > min_impr) {
 			sq_err <- 0.0
-			pred_time <- system.time(0)
-			err_time <- system.time(0)
-			update_time <- system.time(0)
 			
 			for(j in 1:num_nonzeros) {
 				# find predicted val
-				#start_pred <- proc.time()
 				predval <- t(row_feats[,nonzero_rows[j] ]) %*% col_feats[,nonzero_cols[j] ]
-				#pred_time <- pred_time + proc.time() - start_pred
 				
 				# apply cut off
 				if(predval < minval) { predval <- minval }
 				if(predval > maxval) { predval <- maxval }
 				
 				# Find Error
-				#start_err <- proc.time()
 				err <- nonzero_entries[j] - predval
 				sq_err <- sq_err + err*err + k/2.0 * ((row_feats[i, nonzero_rows[j] ])^2) * ((col_feats[i, nonzero_cols[j] ])^2)
-				#err_time <- err_time + proc.time() - start_err
 				
 				# Update row and col features
-				#start_update <- proc.time()
 				new_row_feat <- (1-lrate*k)*row_feats[i, nonzero_rows[j] ] + lrate*err*col_feats[i, nonzero_cols[j] ]
 				new_col_feat <- (1-lrate*k)*col_feats[i, nonzero_cols[j] ] + lrate*err*row_feats[i, nonzero_rows[j] ]
 				row_feats[i, nonzero_rows[j] ] <- new_row_feat
 				col_feats[i, nonzero_cols[j] ] <- new_col_feat
-				#update_time <- update_time + proc.time() - start_update
 			}
-			# Calculate RMSE
+			# Calculate RMSE and improvement
 			rmse_prev <- rmse
 			rmse <- sqrt(sq_err/num_nonzeros)
-			#cat("pred_time: ",pred_time,"\n")
-			#cat("err_time: ",err_time,"\n")
-			#cat("update_time: ",update_time,"\n")
-			#cat("fortime: ", fortime,"\n")
 			cat("root mean squared error: ",rmse)
 			cat("\n")
 			impr <- rmse_prev - rmse
@@ -310,7 +303,7 @@ sgdBase <- function(mat) {
 errorCal <- function(mat, U, V){
 	# Find nonzero entries
 	IIJJ <- which(mat != 0,arr.ind = T)
-	numNonzero <- length(IIJJ)
+	numNonzero <- nnzero(mat)
 	II <- IIJJ[,1]
 	JJ <- IIJJ[,2]
 	mX <- sparseMatrix(i=II,j=JJ,x=maskUV(as.matrix(U),as.matrix(V),II,JJ),dims=dim(mat)) # Call into C++ code
@@ -320,107 +313,81 @@ errorCal <- function(mat, U, V){
 }
 
 # Divide factor combine
-dfc <- function(mat, sc, slices, iters, randProject=TRUE, local = FALSE,writeby = 10) {
-	if(local == FALSE) {
-		sourceCpp('maskUV.cpp')
-		# pick a random permutation of the columns
-		cols <- dim(mat)[2]
-		#sampleCols <- sample(cols)
-	
-		# make the matrix into a list of column chunks
-		listMat <- lapply(1:slices, function(i) list(iters,mat[,(1 + floor((i-1)*cols/slices)):floor(i*cols/slices),drop=FALSE]))
-	
-		# distribute the column slices with spark
-		# might need to pass in desired num slices here?
-	
-		tover <- proc.time()
-		subMatRDD <- parallelize(sc,listMat,slices)
-		overhead <- as.numeric((proc.time() - tover)["elapsed"])
-	
-		# factor each slice
-		factorsRDD <- lapplyPartition(subMatRDD,factorCols)
-	
-		# collect the results and project them onto the first column slice
-		factorList <- collect(factorsRDD)
-		matrixList <- lapply(seq(1,length(factorList)), function(i) list(factorList[[i]][[1]],factorList[[i]][[2]]))
-		subTimeList <- lapply(seq(1,length(factorList)), function(i) factorList[[i]][[3]])
-		subTime <- max(unlist(subTimeList))
-		cat("Time for subproblems: \n")
-		print(subTimeList)
+dfc <- function(mat, sc, slices, iters, randProject=TRUE) {
+	sourceCpp('maskUV.cpp')
 
-		# collect the results and project them onto a low rank matrix
-		if(randProject) {
-			cat("Doing random projection to combine submatrices...\n")
-			result <- dfcRandProject(matrixList)
-		} else { 
-			cat("Projecting all submatrices onto first submatrix...\n")
-			result <- dfcProject(matrixList)
-		}
-		projTime <- result[[3]]
-		cat("Time for collection: ",projTime,"\n")
-	} else {
-		result <- apgBase(mat,iters,local,writeby)
-		overhead <- 0
-		subTime <- result[[3]]
-		projTime <- 0
-	}				
+	# Cut the matrix by columns into several submatrices, one for each slice of computation
+	cols <- dim(mat)[2]	
+	listMat <- lapply(1:slices, function(i) list(iters,mat[,(1 + floor((i-1)*cols/slices)):floor(i*cols/slices),drop=FALSE]))
+	
+	tover <- proc.time() # Timing Code
+	
+	# Create the RDD
+	subMatRDD <- parallelize(sc,listMat,slices)
 		
-	# get the error
-	#print(result[[1]]%*%t(result[[2]]))
+	overhead <- as.numeric((proc.time() - tover)["elapsed"]) # Timing Code
+	
+	# factor each slice
+	factorsRDD <- lapplyPartition(subMatRDD,factorCols)
+	
+	# collect the results
+	factorList <- collect(factorsRDD)
+	matrixList <- lapply(seq(1,length(factorList)), function(i) list(factorList[[i]][[1]],factorList[[i]][[2]]))
+	subTimeList <- lapply(seq(1,length(factorList)), function(i) factorList[[i]][[3]])
+		
+	# Timing Code
+	subTime <- max(unlist(subTimeList))
+	cat("Time for subproblems: \n")
+	print(subTimeList)
+
+	# Collect the results and project them onto a low rank matrix
+	if(randProject) {
+		cat("Doing random projection to combine submatrices...\n")
+		result <- dfcRandProject(matrixList)
+	} else { 
+		cat("Projecting all submatrices onto first submatrix...\n")
+		result <- dfcProject(matrixList)
+	}
+	
+	# Timing Code
+	projTime <- result[[3]]
+	cat("Time for collection: ",projTime,"\n")				
+		
+	# Compute the error in the prediction
 	error <- errorCal( mat, result[[1]], result[[2]])
 	list(error,overhead,subTime,projTime)
 }
 
+# Driver Code #############################################
+
+# Initialize the spark context
 sc <- sparkR.init(args[[1]], "DFCR")
-
 slices <- ifelse(length(args) > 1, as.integer(args[[2]]),2)
-local1 <- F
-if(args[[1]] == "local[1]") {local1 <- T}
-
-
-# Test matrix init
-#dims <- 100
-#r <- 10
-#testU <- matrix(rnorm(r*dims,mean = 4,sd = 1),r,dims)
-#testV <- matrix(rnorm(r*dims,mean = 4,sd = 1),r,dims)
-#testG <- matrix(rnorm(dims*dims,mean = 0,sd = .05),dims,dims)
-#testM <- t(testV) %*% testU + testG
-
-#zeros <- sample.int(dims^2, size=floor(0.7*dims^2)) 
-
-#zeroedM <- testM
-#for(i in zeros){
-	#x = i %% dims
-	#y = i %/% dims
-	#zeroedM[x,y] <- 0
-#}
 
 # Read matrix from file
 maskedFile <- args[[3]]
+maskedM <- readMM(maskedFile)
+dims <- dim(maskedM)[1]
+revealedEntries <- nnzero(maskedM)
+
+# Number of Iterations to run base algorithm
 iterations <- args[[4]]
-if(length(args) > 5) {
-	outfile <- args[[6]]
-}
+
+# Determine Projection method
 randProj <- T
 if(length(args) > 4) {
 	if(args[[5]] == 'F') {
 		randProj <- F
 	}
 }
-#trueVFile <- args[[5]]
 
-maskedM <- readMM(maskedFile)
-dims <- dim(maskedM)[1]
-revealedEntries <- nnzero(maskedM)
-#trueU <- read(trueUFile)
-#trueV <- read(trueVFile)
+ttot <- proc.time() # Timing Code
 
 # Run DFC
+outs <- dfc(maskedM, sc, slices, iterations, randProject=randProj)
 
-ttot <- proc.time()
-outs <- dfc(maskedM, sc, slices, iterations, randProject=randProj,local=local1,writeby=10)
-totalTime <- as.numeric((proc.time() - ttot)["elapsed"])
+totalTime <- as.numeric((proc.time() - ttot)["elapsed"]) # Timing Code
+
 cat("RMSE for the entire matrix: ",outs[[1]],"\n")
 cat("Total time for DFC: ",totalTime,"\n")
-#cat("Average magnitude of entries of M: ",sum(abs(maskedM))/revealedEntries,"\n")
 outs
